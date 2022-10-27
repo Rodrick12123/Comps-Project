4721cbb00275404cab6594e91f6bfb69a6b8fd1d
flemingw <55925069+flemingw@users.noreply.github.com>
2022-10-27 17:07:41 -0500

    functions for setting up canvas, changing color, and changing size

--- canvas.js
+++ canvas.js
@@ -1,6 +1,15 @@
-const myCanvas = new fabric.Canvas("demoCanvas", {
-  width: window.innerWidth - 200,
-  height: window.innerHeight - 100,
-  backgroundColor: "white",
-  isDrawingMode: true,
-});
+//sets initial conditions for canvas
+const canvas = new fabric.Canvas("canvas")
+  canvas.isDrawingMode = true;
+  canvas.freeDrawingBrush.color = 'blue';
+  canvas.freeDrawingBrush.width = 5;
+
+//changes brush color
+function changeColor() {
+  canvas.freeDrawingBrush.color = document.getElementById("colorpicker").value;
+}
+
+//changes brush size
+function changeSize() {
+  canvas.freeDrawingBrush.width = document.getElementById("sizepicker").value;
+}
