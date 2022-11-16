19115ea8d1b52536a2b8ef22d7a4f56cb266b858
Rodrick <lankfordr@carleton.edu>
2022-11-16 16:56:26 -0600

    start timer implementation

--- public/javascript/prompt.js
+++ public/javascript/prompt.js
@@ -2,3 +2,39 @@ function savePrompt() {
     var promptInput = document.getElementById("prompt").value;
     alert(promptInput)
 }
+//this does not work for now
+let minutes = 2; 
+let seconds = 30; 
+
+let totalSeconds = minutes * 60 + seconds;
+
+const convert = (value, inSeconds) => {
+    if (value > inSeconds) {
+        let x = value % inSeconds;
+        tempSeconds = x;
+        return (value - x) / inSeconds;
+    } else {
+        return 0;
+    }
+    };
+
+//sets seconds
+const setSeconds = (s) => {
+    document.querySelector("#seconds").textContent = s + "s";
+};
+    
+    //sets minutes
+const setMinutes = (m) => {
+    document.querySelector("#minutes").textContent = m + "m";
+};
+
+var x = setInterval(() => {
+    //clears countdown when all seconds are counted
+    if (totalSeconds <= 0) {
+        clearInterval(x);
+    }
+    setMinutes(convert(tempSeconds, 60));
+    setSeconds(tempSeconds == 60 ? 59 : tempSeconds);
+    totalSeconds--;
+    tempSeconds = totalSeconds;
+}, 1000);
