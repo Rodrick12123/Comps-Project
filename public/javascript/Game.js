4f5e1af860405f6a27ca3a14cedd36e91abb76a2
30brhee <ninja0130@gmail.com>
2023-02-14 17:32:44 -0600

    Added implementation for disconnection and reconnection

--- public/javascript/Game.js
+++ public/javascript/Game.js
@@ -17,6 +17,7 @@ class Game {
         this.timerStatus = false;
         this.host = host;
         this.numPlayersInWaitRoom = 0;
+        this.disconnectedPlayers = [];
     }
 
 
@@ -33,6 +34,43 @@ class Game {
         this.finishedPlayers.push(username);
     }
 
+    // Adds player to disconnectedPlayers and removes player from players
+    disconnectPlayer(socketID) {
+        for (let i = 0; i < this.players.length; i++) {
+            if (this.players[i].socketID == socketID) {
+                console.log("removing player: " + this.players[i].username);
+                this.disconnectedPlayers.push(this.players[i]); // add player to disconnectedPlayers[]
+                this.players.splice(i, 1); // remove the player from players list
+            }
+        }
+
+        this.numPlayers--;
+        // remove player username from usernames[]
+
+        // console.log("current players: " + this.displayPlayers());
+        console.log("current players length: " + this.players.length);
+        // console.log("current disconnected players: " + this.displayDisconnectedPlayers());
+        console.log("current disconnected players: " + this.disconnectedPlayers.length);
+    }
+
+    // Let's player join any game lobby that already has >0 disconnections during the game
+    // and reconnects the player to the game
+    reconnectPlayer(username, socketID) {
+        if (this.disconnectedPlayers.length > 0) {
+            var newPlayer = this.disconnectedPlayers[0];
+            this.disconnectedPlayers.splice(0, 1);
+        }
+        else {
+            console.log("Error: no player has been disconnected during the game");
+        }
+        newPlayer.username = username; // update player's username
+        newPlayer.socketID = socketID;
+        this.addPlayer(newPlayer);
+        console.log("player reconnecting: " + newPlayer.username);
+    
+        // might need to handle interactions with the re-added player's book object not 
+        // corresponding with current game state
+    }
 
     /* Appends the book to the list of books */
     addBook(book) {
@@ -44,7 +82,6 @@ class Game {
     getTimer(){
         return this.timerStatus;
     }
-
 
     //Probably want get functions for these variables
     /* get function for the current round 
@@ -66,7 +103,27 @@ class Game {
         }
     }
 
-    // Might want some more methods implemented
+    hasDisconnectedPlayers() {
+        if (this.disconnectedPlayers.length > 0) {
+            return true;
+        }
+        else {
+            return false;
+        }
+    }
+
+    displayPlayers() {
+        for(let i=0; i<this.players.length; i++) {
+            console.log(this.players[i].username);
+        }
+    }
+
+    displayDisconnectedPlayers() {
+        for(let i=0; i<this.disconnectedPlayers.length; i++) {
+            console.log(this.disconnectedPlayers[i]);
+        }
+    }
+
 }
 
 module.exports = Game;
